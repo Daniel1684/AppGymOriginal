14e726da4faf8f4d023140c0d4232ab3a02f5f6e
daniel.reimundo <daniel1684@hotmail.com>
2017-06-13 17:07:29 +0200

    Spinner editado

--- app/src/main/java/com/danielreimundo/appgym/Estadisticas.java
+++ app/src/main/java/com/danielreimundo/appgym/Estadisticas.java
@@ -12,6 +12,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.danielreimundo.appgym.Adapter.GymAdapter;
 import com.danielreimundo.appgym.bd.DatabaseHelper;
@@ -19,6 +20,7 @@ import com.danielreimundo.appgym.bd.DatabaseManager;
 import com.danielreimundo.appgym.rutinas.Abdominales;
 import com.danielreimundo.appgym.rutinas.Rutina;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -27,6 +29,11 @@ public class Estadisticas extends AppCompatActivity implements AdapterView.OnIte
     private DatabaseManager db;
     private List<String> lista = new ArrayList<>();
     public Spinner spinni;
+    ///------------
+    private Spinner spinner;
+    private ArrayList<String> data;
+    private HashMap<String, ArrayList<String>> prototype;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +41,14 @@ public class Estadisticas extends AppCompatActivity implements AdapterView.OnIte
         this.setTitle("Listas");
         setContentView(R.layout.activity_estadisticas);
         spinni = (Spinner)findViewById(R.id.spinner_estadistica_spinni);
-
-
         db = new DatabaseManager(this);
-        leerRutina("Flexiones");
-        db.borrar(9);
+       // leerRutina("Flexiones");
+        //db.borrar(9);
 
-        //GymAdapter gymAdapter = new GymAdapter(this, null);
+        loadData();
+        loadEvent();
 
-       // GymAdapter gymAdapter = new ArrayList<String>(this, android.R.layout.simple_expandable_list_item_1,lista);
-        //ListView lv =(ListView)findViewById(android.R.id.list);
-
-        //lv.setAdapter(gymAdapter);
-
-
+/*
         spinni.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -85,9 +86,10 @@ public class Estadisticas extends AppCompatActivity implements AdapterView.OnIte
                 // sometimes you need nothing here
             }
         });
+        */
     }
 
-
+/*
     public List<String> leerRutina(String rut){
 
         Cursor c = db.getRutina(rut);
@@ -102,16 +104,72 @@ public class Estadisticas extends AppCompatActivity implements AdapterView.OnIte
         System.out.println(lista.size());
         return lista;
     }
-
-
+    */
     @Override
     protected void onStart(){
         super.onStart();
 
     }
-
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
+
+
+    //---------------------------------
+
+    private void loadData(){
+        data = new ArrayList<>();
+        data.add("Abdominales");
+        data.add("Correr");
+        data.add("Flexiones");
+        data.add("Sentadillas");
+        prototype = new HashMap<>();
+        ArrayList<String> abdom = new ArrayList<>();
+        Cursor c = db.getRutina("Abdominales");
+        while (c.moveToNext()){
+            for(int i = 0; i < 10; i++){
+                abdom.add(c.getString(i));
+            }
+        }
+        prototype.put("Abdominales",abdom);
+
+        ArrayList<String> corre = new ArrayList<>();
+        Cursor b =db.getRutina("Correr");
+        while (b.moveToNext()){
+            for(int i = 0; i < 10; i++){
+                corre.add(b.getString(i));
+            }
+        }
+        prototype.put("Correr",corre);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    }
+    private void loadEvent(){
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value = String.valueOf(parent.getItemAtPosition(position));
+                String phrase = String.format("Hahahahahahahah: ",value);
+                Toast.makeText(parent.getContext(),phrase, Toast.LENGTH_SHORT).show();
+                ListView listView = (ListView)findViewById(R.id.listview_estadisticas);
+                String key = data.get(position);
+                ArrayList<String> unique = prototype.get(key);
+                GymAdapter gymAdapter = new GymAdapter(parent.getContext(), unique);
+                listView.setAdapter(gymAdapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+
 }
